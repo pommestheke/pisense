e9e63231845b6ebbeeb3c3c786e3877334ea0804
Kai Welke <kwelke@gmx.de>
2021-03-01 23:11:22 -0800

    Added deviceId to telemetry dataset

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pommestheke/pisense
 
 go 1.16
 
-require github.com/amenzhinsky/iothub v0.7.0 // indirect
+require (
+	github.com/amenzhinsky/iothub v0.7.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+)
--- sensor/telemetry.go
+++ sensor/telemetry.go
@@ -4,10 +4,12 @@ package sensor
 import (
 	"math/rand"
 	"time"
+	"github.com/satori/go.uuid"
 )
 
 // Telemetry type holding all sensory data
 type Telemetry struct {
+	DeviceId string `json:"deviceId"`
 	Timestamp string `json:"timestamp"`
 	Value     int    `json:"value"`
 }
@@ -15,18 +17,20 @@ type Telemetry struct {
 // Sensor type for reading out values
 type Sensor struct {
 	maximum int
+	deviceId string
 	rand    *rand.Rand
 }
 
 // Create a new sensor which allows reading out of values
 func CreateSensor(maximum int, source rand.Source) Sensor {
-	s := Sensor{maximum, rand.New(source)}
+	s := Sensor{maximum, uuid.NewV4().String(), rand.New(source)}
 	return s
 }
 
 // Read out the sensor
 func (s Sensor) ReadOut() (t Telemetry) {
 	t = Telemetry{
+		DeviceId: s.deviceId,
 		Timestamp: time.Now().String(),
 		Value:     s.rand.Intn(s.maximum),
 	}
